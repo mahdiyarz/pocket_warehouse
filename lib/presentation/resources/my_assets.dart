02e0f42d6ddcd8b88ad09e7052bfae67bcc60ad2
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-09 22:43:49 +0430

    feat: create my assets resource class

---
+++ lib/presentation/resources/my_assets.dart
@@ -0,0 +1,13 @@
+const String imagePath = 'assets/images';
+
+abstract class MyImageAssets {
+  static const String notFoundPageRoute = '$imagePath/not_found.png';
+  static const String incomingListScreen = '$imagePath/new_entries.png';
+  static const String incomingGoodsScreen = '$imagePath/incoming_goods.png';
+  static const String countGoodsScreen = '$imagePath/count_goods.png';
+  static const String homeScreen = '$imagePath/home.png';
+  static const String goodsScreen = '$imagePath/empty_goods.png';
+  static const String brandsScreen = '$imagePath/mystery_brand.png';
+  static const String personScreen = '$imagePath/no_person.png';
+  static const String warehouseScreen = '$imagePath/warehouse_management.png';
+}
